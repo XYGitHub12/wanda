daa9c7b08e2a49a92ab432875c687fe833f782fa
xueyu <1375156056@qq.com>
2019-06-18 17:14:51 +0800

    MyBatis逆向工程测试

---
+++ wanda-manager/wanda-manager-dao/src/test/java/com/qf/manager/dao/TbItemMapperTest.java
@@ -0,0 +1,21 @@
+package com.qf.manager.dao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.*;
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:spring/spring-dao-test.xml"})
+public class TbItemMapperTest {
+
+    @Autowired
+    private TbItemMapper itemDao;
+    @Test
+    public void selectByPrimaryKey() throws Exception {
+        System.out.println(itemDao.selectByPrimaryKey(562379L));
+    }
+
+}
